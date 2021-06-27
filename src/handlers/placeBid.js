253b955ac9e168ac88507e6217b3869360f42edd
sanjevscet <sanjeevscet@gmail.com>
2021-06-27 20:25:10 +0530

    chapter 4 completed

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -2,13 +2,23 @@ import AWS from "aws-sdk";
 import commonMiddleware from "../lib/commonMiddleware";
 import createError from "http-errors";
 
+import { getAuctionById } from "./getAuction";
+
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function placeBid(event, context) {
   const { id } = event.pathParameters;
   const { amount } = event.body;
-  console.log(amount);
-  console.log({ body: event });
+  console.log({ amount });
+
+  const auction = await getAuctionById(id);
+  console.log({ auction });
+
+  if (amount <= auction.highestBid.amount) {
+    throw new createError.Forbidden(
+      `Your bid must be greater than ${auction.highestBid.amount}`
+    );
+  }
 
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
