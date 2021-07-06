009683d2b0c63ce8831417f8723a6c8babd5d6d0
sanjevscet <sanjeevscet@gmail.com>
2021-07-06 17:19:40 +0530

    Chapter 5-3 completed

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,5 +1,9 @@
+import { getEndedAuction } from "../lib/getEndedAuctions";
+
 async function processAuctions(event, context) {
   console.log("processing auctions");
+  const auctionToClose = await getEndedAuction();
+  console.log(auctionToClose);
 }
 
 export const handler = processAuctions;
